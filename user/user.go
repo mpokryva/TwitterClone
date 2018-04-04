1d99eefa108eda7302eb65d5c56384f2ed91cdb1
root <root@compas.cs.stonybrook.edu>
2018-04-04 04:56:33 +0000

    Added following and followers string array type fields to user. Modified JSON marshaling schema.

--- user/user.go
+++ user/user.go
@@ -4,8 +4,10 @@ type User struct {
     Username string `json:"username,omitempty" bson:"username"`
     Email string `json:"email,omitempty" bson:"email"`
     Password string `json:"password,omitempty" bson:"password"`
-    Followers int `json:"followers" bson:"followers"`
-    Following int `json:"following" bson:"following"`
+    Followers []string `json:"followers" bson:"followers"`
+    Following []string `json:"following" bson:"following"`
+    FollowerCount []string `json:"followerCount" bson:"followerCount"`
+    FollowingCount []string `json:"followingCount" bson:"followingCount"`
     Verified bool `json:"verified,omitempty" bson:"verified"`
     Key string `json:"key,omitempty" bson:"key"`
 }
