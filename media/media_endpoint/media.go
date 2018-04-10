7881b249d34ca620bd89bb27130e17225cae08b6
Ubuntu <ubuntu@compas.cs.stonybrook.edu>
2018-04-10 05:27:05 +0000

    GET media endpoint now responding with correct header.

--- media/media_endpoint/media.go
+++ media/media_endpoint/media.go
@@ -4,8 +4,9 @@ import (
     "context"
     "net/http"
     "os"
+    "errors"
+    "strconv"
     "github.com/sirupsen/logrus"
-    "encoding/json"
     "github.com/gorilla/mux"
     "github.com/mongodb/mongo-go-driver/bson/objectid"
     "github.com/mongodb/mongo-go-driver/bson"
@@ -37,7 +38,7 @@ func mediaHandler(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     id := vars["id"]
     log.Debug(id)
-    var media []byte
+    var media media.Media
     oid, err := objectid.FromHex(id)
     if err != nil {
         log.Error(err)
@@ -50,23 +51,28 @@ func mediaHandler(w http.ResponseWriter, r *http.Request) {
     encodeResponse(w, media)
 }
 
-func getMedia(oid objectid.ObjectID) ([]byte, error) {
+func getMedia(oid objectid.ObjectID) (media.Media, error) {
+    var nilMedia media.Media
     client, err := wrappers.NewClient()
     if err != nil {
-        return nil, err
+        return nilMedia, err
     }
     db := client.Database("twitter")
     coll := db.Collection("media")
     var media media.Media
     filter := bson.NewDocument(bson.EC.ObjectID("_id", oid))
     err = coll.FindOne(context.Background(), filter).Decode(&media)
-    log.Debug(media)
-    return media.Content, err
+    return media, err
 }
 
-func encodeResponse(w http.ResponseWriter, response interface{}) error {
-    w.Header().Set("Content-Type", "application/octet-stream")
-    return json.NewEncoder(w).Encode(response)
+func encodeResponse(w http.ResponseWriter, m media.Media) {
+    if (m.Content == nil) {
+        err := errors.New("Media not found.")
+        log.Debug(err)
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+    w.Header().Set("Content-Type", m.Header.Header["Content-Type"][0])
+    w.Header().Set("Content-Length", strconv.Itoa(len(m.Content)))
+    w.Write(m.Content)
 }
-
-
