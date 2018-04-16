8cbf1e883fabc686685c71036f9031fbd691d333
Ubuntu <ubuntu@ubuntu-project.cloud.compas.cs.stonybrook.edu>
2018-04-16 00:09:03 +0000

    Injected logger (not yet functional).

--- main_server/main_server.go
+++ main_server/main_server.go
@@ -3,10 +3,9 @@ package main
 import (
     "log"
     "net/http"
-    "github.com/sirupsen/logrus"
+    "github.com/sirupsen/Logrus"
     "TwitterClone/wrappers"
     "TwitterClone/additem"
-    /*
     "TwitterClone/user/user_endpoints"
     "TwitterClone/user/user_endpoints/followInfo"
     "TwitterClone/user/user_endpoints/adduser"
@@ -16,28 +15,42 @@ import (
     "TwitterClone/follow"
     "TwitterClone/item/item_endpoints"
     "TwitterClone/verify"
-    "TwitterClone/login"
-    "TwitterClone/logout"
-    */
+    "TwitterClone/Login"
+    "TwitterClone/Logout"
 )
 
-var Log *logrus.Logger
+var Log *Logrus.Logger
 
 func main() {
     // Log to a file
     var f *os.File
     var err error
-    Log, f, err = wrappers.FileLogger("central.log", os.O_CREATE | os.O_RDWR,
+    Log, f, err = wrappers.FileLogger("central.Log", os.O_CREATE | os.O_RDWR,
         0666)
     if err != nil {
-        log.Fatal("Logging file could not be opened.")
+        Log.Fatal("Logging file could not be opened.")
     }
     f.Truncate(0)
     f.Seek(0, 0)
     defer f.Close()
-    log.SetLevel(logrus.ErrorLevel)
-    additem.Log = Log
+    Log.SetLevel(Logrus.ErrorLevel)
+    injectLogger()
     router := NewRouter()
     http.Handle("/", router)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    Log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func injectLogger() {
+    additem.Log = Log
+    user_endpoints.Log = Log
+    followInfo.Log = Log
+    addUser.Log = Log
+    search.Log = Log
+    addmedia.Log = Log
+    media_endpoints.Log = Log
+    follow.Log = Log
+    item_endpoints.Log = Log
+    verify.Log = Log
+    Login.Log = Log
+    Logout.Log = Log
 }
