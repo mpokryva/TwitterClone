84243975e2ad9f27be73b7c9ec99973d8b2b5043
Ubuntu <ubuntu@ubuntu-project.cloud.compas.cs.stonybrook.edu>
2018-04-17 07:31:09 +0000

    Verify works with backdoor.

--- verify/verify.go
+++ verify/verify.go
@@ -3,21 +3,24 @@ package verify
 import (
     "context"
     "time"
-    logrus "github.com/sirupsen/logrus"
+    "errors"
+    "github.com/sirupsen/logrus"
     "net/http"
     "encoding/json"
     "github.com/mongodb/mongo-go-driver/bson"
+    "github.com/mongodb/mongo-go-driver/bson/objectid"
     "TwitterClone/wrappers"
+    "TwitterClone/user"
 )
 
 type verification struct {
-  Key *string `json:"key"`
-  Email *string `json:"email"`
+    Key *string `json:"key"`
+    Email *string `json:"email"`
 }
 
 type res struct {
-  Status string `json:"status"`
-  Error string `json:"error,omitempty"`
+    Status string `json:"status"`
+    Error string `json:"error,omitempty"`
 }
 var Log *logrus.Logger
 func main() {
@@ -26,7 +29,7 @@ func main() {
 
 
 func VerifyHandler(w http.ResponseWriter, req *http.Request) {
-  start := time.Now()
+    start := time.Now()
     decoder := json.NewDecoder(req.Body)
     var verif verification
     var r res
@@ -36,21 +39,21 @@ func VerifyHandler(w http.ResponseWriter, req *http.Request) {
     }
     valid := validateParams(verif)
     if valid {
-      if(user_exists(verif)){
-          r.Status = "OK"
-      }else {
-        Log.Error("Input not valid!")
+        if(verifyUser(verif)){
+            r.Status = "OK"
+        }else {
+            Log.Error("Input not valid!")
+            r.Status = "error"
+            r.Error = "Could not complete verification"
+        }
+    }else{
         r.Status = "error"
-        r.Error = "Could not complete verification"
-      }
-  }else{
-    r.Status = "error"
-    r.Error = "Not enough input"
-  }
+        r.Error = "Not enough input"
+    }
 
-  elapsed := time.Since(start)
-  Log.Info("Verify elapsed: " + elapsed.String())
-  json.NewEncoder(w).Encode(r)
+    elapsed := time.Since(start)
+    Log.Info("Verify elapsed: " + elapsed.String())
+    json.NewEncoder(w).Encode(r)
 }
 
 func validateParams(verif verification) bool {
@@ -67,29 +70,83 @@ func validateParams(verif verification) bool {
     return valid
 }
 
-func user_exists(verif verification) bool {
-    dbStart := time.Now()
+func verifyUser(verif verification) bool {
     client, err := wrappers.NewClient()
     if err != nil {
-        Log.Error("Mongodb error")
+        Log.Error(err)
         return false
     }
     db := client.Database("twitter")
-    col := db.Collection("users")
-    filter := bson.NewDocument(bson.EC.String("email", *verif.Email),
-            bson.EC.String("key", "<" + *verif.Key + ">"),
-            bson.EC.Boolean("verified", false))
+    coll := db.Collection("emails")
+    // Find user.
+    filter := bson.NewDocument(bson.EC.String("email", *verif.Email))
+    result := bson.NewDocument()
+    err = coll.FindOne(context.Background(), filter).Decode(result)
+    elem, err := result.Lookup("_id")
+    if err != nil {
+        Log.Error(err)
+        return false
+    }
+    oid := elem.Value().ObjectID()
+    if *verif.Key == "abracadabra" {
+        err = mongoUpdateVerify(oid)
+        if err != nil {
+            Log.Error(err)
+        }
+        return err == nil
+    }
+    var user user.User
+    coll = db.Collection("users")
+    err = coll.FindOne(context.Background(), filter).Decode(&user)
+    if err != nil {
+        Log.Error(err)
+        return false
+    } else {
+        Log.Debug(user)
+    }
+    if user.Key == "<" + *verif.Key + ">" {
+        // Verification keys match.
+        err = mongoUpdateVerify(oid)
+        if err != nil {
+            Log.Error(err)
+        }
+        return err == nil
+    } else {
+        Log.Info("Verification keys do not match.")
+        return false
+    }
+}
+
+func mongoUpdateVerify(userID objectid.ObjectID) error {
+    dbStart := time.Now()
+    client, err := wrappers.NewClient()
+    if err != nil {
+        Log.Error(err)
+        return err
+    }
+    db := client.Database("twitter")
+    coll := db.Collection("users")
+    filter := bson.NewDocument(bson.EC.ObjectID("_id", userID))
     update := bson.NewDocument(
         bson.EC.SubDocumentFromElements("$set",
         bson.EC.Boolean("verified", true)))
-    result, err := col.UpdateMany(
+    result, err := coll.UpdateOne(
         context.Background(),
         filter, update)
     if err != nil {
-      elapsed := time.Since(dbStart)
-      Log.WithFields(logrus.Fields{"msg":"Check if user exists time elapsed", "timeElapsed":elapsed.String()}).Error(err)
+        elapsed := time.Since(dbStart)
+        Log.WithFields(logrus.Fields{"msg":"Check if user exists time elapsed",
+        "timeElapsed":elapsed.String()}).Error(err)
+        return err
     }
-      elapsed := time.Since(dbStart)
-      Log.WithFields(logrus.Fields{"msg":"Check if user exists time elapsed", "timeElapsed":elapsed.String()}).Info()
-    return result.ModifiedCount == 1
+    elapsed := time.Since(dbStart)
+    Log.WithFields(logrus.Fields{"msg":"Check if user exists time elapsed",
+    "timeElapsed":elapsed.String()}).Info()
+    if result.ModifiedCount == 1 {
+        return nil
+    } else {
+        err = errors.New("ModifiedCount == 0... Something weird happened.")
+        Log.Error(err)
+        return err
+    }
 }
