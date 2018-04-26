573e0987a7cf13115605065c2a56f46c8de0ab4e
Ubuntu <ubuntu@ubuntu-project.cloud.compas.cs.stonybrook.edu>
2018-04-26 19:49:11 +0000

    Optimized addmedia to return earlier.

--- media/addmedia/addmedia.go
+++ media/addmedia/addmedia.go
@@ -4,7 +4,7 @@ import (
     "context"
     "net/http"
     "time"
-
+    "errors"
     "io"
     "bytes"
     "github.com/sirupsen/logrus"
@@ -13,6 +13,11 @@ import (
     "TwitterClone/wrappers"
     "TwitterClone/media"
     "TwitterClone/memcached"
+)
+
+const(
+    fileKey = "content"
+    defaultMaxMemory = 32 << 20 // 32 MB
 )
 
 type response struct {
@@ -48,50 +53,60 @@ func errResponse(err error) response {
 }
 
 func AddMediaHandler(w http.ResponseWriter, r *http.Request) {
-  start := time.Now()
-    var res response
+    start := time.Now()
     username, err := checkLogin(r)
     if err != nil {
         Log.Error(err)
         encodeResponse(w, errResponse(err))
         return
     }
-    content, header, err := r.FormFile("content") // Get binary payload.
-    if err != nil {
-        Log.Error(err)
+    if r.MultipartForm == nil {
+        err = r.ParseMultipartForm(defaultMaxMemory)
+        if err != nil {
+            encodeResponse(w, errResponse(err))
+            return
+        }
+    }
+    if r.MultipartForm.File == nil || r.MultipartForm.File[fileKey] == nil {
+        err = errors.New("No file found")
         encodeResponse(w, errResponse(err))
         return
     }
+    // File exists.
+    oid := objectid.New()
+    elapsed := time.Since(start)
+    Log.Info("Add Media (pre-insert) elapsed: " + elapsed.String())
+    var res response
+    res.Status = "OK"
+    res.ID = oid.Hex()
+    encodeResponse(w, res)
+    go insertWithTimer(r, oid, username, start)
+}
+
+func insertWithTimer(r *http.Request, oid objectid.ObjectID,
+    username string, start time.Time) {
+    content, header, err := r.FormFile(fileKey) // Get binary payload.
+    if err != nil {
+        Log.Error(err)
+        return
+    }
+    var m media.Media
+    m.ID = oid
     defer content.Close()
     Log.Debug(header.Header)
     bufContent := bytes.NewBuffer(nil)
     if _, err := io.Copy(bufContent, content); err != nil {
         Log.Error(err)
-        encodeResponse(w, errResponse(err))
         return
     }
     buf := bufContent.Bytes()
-    var m media.Media
     if header != nil {
         m.ContentType = header.Header["Content-Type"][0]
     }
     m.Content = buf
     m.Username = username
     // Add the Media.
-    oid := objectid.New()
-    m.ID = oid
-    elapsed := time.Since(start)
-    Log.Info("Add Media (pre-insert) elapsed: " + elapsed.String())
-    res.Status = "OK"
-    res.ID = oid.Hex()
-    encodeResponse(w, res)
-    elapsed = time.Since(start)
-    Log.Info("Add Media (post-response) elapsed: " + elapsed.String())
-    go insertWithTimer(m, start)
-}
-
-func insertWithTimer(m media.Media, start time.Time) {
-    err := insertMedia(m)
+    err = insertMedia(m)
     if err != nil {
        Log.Error(err.Error())
     }
